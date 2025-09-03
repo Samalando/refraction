4feeda0a6fde5822d084c212a0c465b091674fe6
Samalando <156577273+Samalando@users.noreply.github.com>
2025-09-03 19:02:47 +0100

    Update support_onboard.rs to contain faq
    uhhhhhhhh yes
    
    Signed-off-by: Samalando <156577273+Samalando@users.noreply.github.com>

--- src/handlers/event/support_onboard.rs
+++ src/handlers/event/support_onboard.rs
@@ -34,6 +34,7 @@ pub async fn handle(ctx: &Context, thread: &GuildChannel) -> Result<()> {
 	let msg = format!(
     "<@{}> We've received your support ticket! {} {}",
     owner,
+	"Please check the [faq](https://discord.com/channels/1031648380885147709/1409111583342202951)!",
     "Please upload your logs and post the link here if possible (run `/tag log` to find out how).",
     "Please don't ping people for support questions, unless you have their permission."
     );
